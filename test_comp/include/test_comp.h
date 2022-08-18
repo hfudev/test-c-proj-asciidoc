4a79de07a853dca6e10efdd1ac537ad17d916b97
Fu Hanxi <fuhanxi@espressif.com>
2022-08-18 14:17:57 +0800

    add a idf component

---
+++ test_comp/include/test_comp.h
@@ -0,0 +1,14 @@
+
+/**
+ * @brief print function
+ * 
+ */
+void print(void);
+
+/**
+ * @brief add function
+ * 
+ * @param a a
+ * @param b b
+ */
+void add(int a, int b);
---
+++ test_comp/test_comp.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "include/test_comp.h"
+
+void print(void)
+{
+    printf("hello world");
+}
+
+void add(int a, int b)
+{
+    printf("%d", a + b);
+}
